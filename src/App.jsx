c3af40c57db1d2f6a452d0c2612f9342245b0f58
mothepro12 <mohamedalaazx@gmail.com>
2023-12-22 01:35:10 -0500

    Describe your changes here

--- src/App.jsx
+++ src/App.jsx
@@ -9,8 +9,8 @@ import Coin from './Routes/Coin';
 function App() {
 
   const [coins, setCoins] = useState([]);
+  const [api] = useState("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false");
 
-  const api = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false";
 
   useEffect(() => {
     axios.get(api).then((response) => {
